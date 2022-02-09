9090aa61b510182802a8d90f7778b56a97bf4cbf
trbKnl <ncdeschipper@gmail.com>
2022-02-09 22:12:50 +0100

    update

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copy all files to the VM for testing
-# recursive, preserve modification times, quite
-rsync -av --delete -e "ssh" /home/turbo/pythontest/securechat root@192.168.111.101:/root
-rsync -av --delete -e "ssh" /home/turbo/pythontest/securechat root@192.168.222.101:/root
-
-echo "done"
